958f59bf1f5acc64ca0561f23ce6945494292f82
Ivan <ivanrakach@gmail.com>
2022-05-25 19:46:05 +0300

    + baton

--- HW_Django_1/HW_Django_1/HW_Django_1/settings.py
+++ HW_Django_1/HW_Django_1/HW_Django_1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'baton',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'website',
     'private_room',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
