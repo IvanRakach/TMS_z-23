ed5e16821698fa7acb6b4919037d6d58d2c6383d
Ivan <ivanrakach@gmail.com>
2022-05-30 08:57:06 +0300

    url_for

--- HW_Flask/flask_test_1.py
+++ HW_Flask/flask_test_1.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)  # __name__ - имя нашего приложения / файла
 
@@ -8,11 +8,14 @@ menu = ["Установка", "Первое приложение", "Обратн
 @app.route("/index")
 @app.route("/")
 def index():
+    # index - имя функции; вызывается в контексте обработчика запроса - функции запроса (def index)
+    print(url_for("index"))
     return render_template("index.html", menu=menu)
 
 
 @app.route("/about")
 def about():
+    print(url_for("about"))  # about - имя функции
     return render_template("about.html", title="О сайте", menu=menu)
 
 
@@ -20,7 +23,38 @@ def about():
 def contact():
     return render_template("contact.html", title="contact", menu=menu)
 
+
+@app.route("/profile/<username>")
+# path: - это конвертер и он говорит о том, что все,
+# что указано после "profile" нужно поместить в переменную "username"
+# int: - это конвертер; должны присутствовать только цифры
+# float: - это конвертер; числа с плавающей точкой
+# path: - это конвертер; любые допустимые символы URL плюс символ "/"
+# @app.route("/profile/<int:username>/<path>")
+# def profile(username, path):
+#     return f"Пользователь: {username}, {path}"
+def profile(username):
+    return f"Пользователь: {username}"
+
+
 # когда мы запускаем лок веб сервер name принимает значение main
 # на удаленном сервере main писать не надо, надо писать имя файла
-if __name__ == "__main__":
-    app.run(debug=True)  # запуск локального вебсервера
+
+# if __name__ == "__main__":
+#     app.run(debug=True)  # запуск локального вебсервера
+
+
+
+# Если нам необходимо осуществить проверку функции url_for для разных обработчиков
+# Фреймворк Flask позволяет нам это сделать искусственно БЕЗ АКТИВАЦИИ ВЕБ СЕРВЕРА:
+# комменитруем след строки:
+# if __name__ == "__main__":
+#     app.run(debug=True)  # запуск локального вебсервера
+
+# вместо них пишем:
+# with - менеджер контекста
+# test_request_context - создаем тестовый контекст запроса
+with app.test_request_context():
+    print(url_for("index"))
+    print(url_for("about"))
+    print(url_for("profile", username="123abcd"))
