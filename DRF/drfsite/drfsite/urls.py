247ef6f713cb31f5d0f036d0052288652d641311
Ivan <ivanrakach@gmail.com>
2022-10-20 21:49:48 +0300

    viewsets + SimpleRouter (part 2)

--- DRF/drfsite/drfsite/urls.py
+++ DRF/drfsite/drfsite/urls.py
@@ -22,8 +22,9 @@ from rest_framework import routers
 
 from women.views import WomenViewSet
 
-router = routers.SimpleRouter()
-router.register(r'women', WomenViewSet)
+router = routers.DefaultRouter()
+router.register(r'women', WomenViewSet, basename='women')
+print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
